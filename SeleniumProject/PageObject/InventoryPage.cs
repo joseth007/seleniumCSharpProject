83710a66830fce3cf5f87052261adc975ca4a71c
Joseth <joseth.cr@gmail.com>
2024-01-30 11:33:32 -0600

    selenium c# project

---
+++ SeleniumProject/PageObject/InventoryPage.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using SeleniumProject.Handler;
+using System;
+
+namespace SeleniumProject.PageObject
+{
+    /*
+     * Clase para representar la pagina del inventario
+     */
+    public class InventoryPage
+    {
+        protected IWebDriver Driver;
+
+        //Locators
+        protected By Title = By.ClassName("title");
+
+        public InventoryPage(IWebDriver driver)
+        {
+            Driver = driver;
+            if(!Driver.Title.Equals("Swag Labs"))
+            {
+                throw new Exception("This is not the inventory page");
+            }
+        }
+
+        public bool TitleIsPresent()
+        {
+            return WaitHandler.ElementIsPresent(Driver, Title);
+        }
+
+
+    }
+
+}
---
+++ SeleniumProject/PageObject/LoginPage.cs
@@ -0,0 +1,54 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace SeleniumProject.PageObject
+{
+    /*
+     * Clase para representar la pagina del Login
+     */
+    public class LoginPage
+    {
+        //Selenium Driver
+        protected IWebDriver Driver;
+
+        //Locators
+        protected By UserInput = By.Id("user-name");
+        protected By PassInput = By.Id("password");
+        protected By LoginButton = By.Id("login-button");
+
+        public LoginPage(IWebDriver driver)
+        {
+            Driver = driver;
+            if (!Driver.Title.Equals("Swag Labs"))
+            {
+                throw new Exception("This is not the login page");
+            }
+        }
+
+        //Metodo para ingresar el usuario
+        public void TypeUserName(String user)
+        {
+            Driver.FindElement(UserInput).SendKeys(user);
+        }
+        //Metodo para ingresar el password
+        public void TypePassword(String pass)
+        {
+            Driver.FindElement(PassInput).SendKeys(pass);
+        }
+
+        //Metodo para clicar el login button
+        public void ClickLoginButton()
+        {
+            Driver.FindElement(LoginButton).Click();
+        }
+
+        public InventoryPage LoginAs(string user, string password)
+        {
+            TypeUserName(user);
+            TypePassword(password);
+            ClickLoginButton();
+            return new InventoryPage(Driver);
+        }
+
+    }
+}
